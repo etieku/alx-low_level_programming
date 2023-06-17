c6823d93f72ebb1107d276d35a112b7acb059be6
Ernest Tieku <ernesttieku8@gmail.com>
2023-06-17 09:50:16 -0700

    6-print_numbers.c

---
+++ 0x01-variables_if_else_while/6-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Single digit numbers of base 10 starting from 0
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int SingleNumBaseTen;
+
+	for (SingleNumBaseTen = 0; SingleNumBaseTen < 10; SingleNumBaseTen++)
+	{
+		putchar ((SingleNumBaseTen % 10) + '0');
+	}
+	printf("\n");
+
+	return (0);
+}
