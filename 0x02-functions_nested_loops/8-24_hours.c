61e09fcf508843f710a6f00c0db09ec54294e2b7
Ernest Tieku <ernesttieku8@gmail.com>
2023-06-20 21:46:08 -0700

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -17,11 +17,11 @@ void jack_bauer(void)
 	{
 	if (a >= 2 && b >= 4)
 		break;
-	_putchar(a + '0');
-	_putchar(b + '0');
+	_putchar(a + 48);
+	_putchar(b + 48);
 	_putchar(58);
-	_putchar(c + '0');
-	_putchar(d + '0');
+	_putchar(c + 48);
+	_putchar(d + 48);
 	_putchar('\n');
 	}
 	}
