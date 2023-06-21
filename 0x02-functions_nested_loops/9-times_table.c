e713ea577d231a818cb0e9622812c22387a7fce6
Ernest Tieku <ernesttieku8@gmail.com>
2023-06-20 22:19:10 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -29,7 +29,7 @@ void times_table(void)
 			else
 				_putchar((prod / 10) + 48); /* get the first digit*/
 
-			_putchar((prod / 10) + 48); /* get the second number*/
+			_putchar((prod % 10) + 48); /* get the second number*/
 		}
 		_putchar('\n');
 	}
